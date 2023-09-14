86c534c72a6f61e1a33f5f952fff0f51e45308f1
Fujita Tetsuya <t-tefujita@yahoo-corp.jp>
2023-09-14 16:31:30 +0900

    社名変更対応 仕様変更

--- yjloginsdk/src/main/java/jp/co/yahoo/yconnect/yjloginsdk/activity/LoginProcessActivity.kt
+++ yjloginsdk/src/main/java/jp/co/yahoo/yconnect/yjloginsdk/activity/LoginProcessActivity.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 LY Corporation
+ * © 2023 LY Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
